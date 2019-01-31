17c62aacc31b92196f279b4e34322e86bb2045b1
Patrick Hogan <phogan110@live.com>
2019-01-30 23:52:30 -0700

    fixed import

--- src/client/Components/App.tsx
+++ src/client/Components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import "./scss/app";
+import "../scss/app.scss";
 
 export default class App extends React.Component<IAppProps, IAppState> {
   constructor(props: IAppProps) {
