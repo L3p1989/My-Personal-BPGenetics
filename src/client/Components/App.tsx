d1e0a43e30733e3bbc7b3e96eea2cb3523afac96
Patrick Hogan <phogan110@live.com>
2019-01-31 17:14:51 -0700

    removed useless code

--- src/client/Components/App.tsx
+++ src/client/Components/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import NavBar from "./NavBar";
-import MorphCards from "./MorphCards";
 
 import "../scss/app.scss";
 
@@ -8,20 +7,13 @@ export default class App extends React.Component<IAppProps, IAppState> {
   constructor(props: IAppProps) {
     super(props);
 
-    this.state = { geneCombos: [] };
-  }
-
-  async componentWillMount() {
-    let r = await fetch("/api/morphs");
-    let geneCombos = await r.json();
-    this.setState({ geneCombos });
+    this.state = {};
   }
 
   render() {
     return (
       <>
         <NavBar />
-        <MorphCards />
       </>
     );
   }
@@ -29,16 +21,4 @@ export default class App extends React.Component<IAppProps, IAppState> {
 
 interface IAppProps {}
 
-interface IAppState {
-  geneCombos: Array<{
-    Name: string;
-    Description: string;
-    Gene1: number;
-    Gene2: number;
-    Gene3: number;
-    Gene4: number;
-    Gene5: number;
-    Gene6: number;
-    URL: string;
-  }>;
-}
+interface IAppState {}
