7320e3e8ae7e33c6095e3b212edb859575305e02
Patrick Hogan <phogan110@live.com>
2019-02-01 11:58:28 -0700

    changed select statements to grab GeneTypes

--- src/server/db/genecombos.ts
+++ src/server/db/genecombos.ts
@@ -3,7 +3,7 @@ import { Connection } from "./index";
 export const all = async () => {
   return new Promise((resolve, reject) => {
     Connection.query(
-      "select gc.Name, gc.Description, bg1.Name as Gene1, bg2.Name as Gene2, bg3.Name as Gene3, bg4.Name as Gene4, bg5.Name as Gene5, bg6.Name as Gene6, gc.URL from genecombos gc left join basicgenetics bg1 on bg1.id = gc.Gene1 left join basicgenetics bg2 on bg2.id = gc.Gene2 left join basicgenetics bg3 on bg3.id = gc.Gene3 left join basicgenetics bg4 on bg4.id = gc.Gene4 left join basicgenetics bg5 on bg5.id = gc.Gene5 left join basicgenetics bg6 on bg6.id = gc.Gene6;",
+      "SELECT gc.Name, gc.Description, gt1.Name AS GeneType1, bg1.Name AS Gene1, gt2.Name AS GeneType2, bg2.Name AS Gene2, gt3.Name AS GeneType3, bg3.Name AS Gene3, gt4.Name AS GeneType4, bg4.Name AS Gene4, gt5.Name AS GeneType5, bg5.Name AS Gene5, gt6.Name AS GeneType6, bg6.Name AS Gene6, gc.URL FROM genecombos gc LEFT JOIN basicgenetics bg1 ON bg1.ID = gc.Gene1 LEFT JOIN basicgenetics bg2 ON bg2.ID = gc.Gene2 LEFT JOIN basicgenetics bg3 ON bg3.ID = gc.Gene3 LEFT JOIN basicgenetics bg4 ON bg4.ID = gc.Gene4 LEFT JOIN basicgenetics bg5 ON bg5.ID = gc.Gene5 LEFT JOIN basicgenetics bg6 ON bg6.ID = gc.Gene6 LEFT JOIN genetypes gt1 ON gt1.ID = bg1.GeneType LEFT JOIN genetypes gt2 ON gt2.ID = bg2.GeneType LEFT JOIN genetypes gt3 ON gt3.ID = bg3.GeneType LEFT JOIN genetypes gt4 ON gt4.ID = bg4.GeneType LEFT JOIN genetypes gt5 ON gt5.ID = bg5.Genetype LEFT JOIN genetypes gt6 ON gt6.ID = bg6.GeneType;",
       (err, results) => {
         if (err) {
           return reject(err);
@@ -17,7 +17,7 @@ export const all = async () => {
 export const one = async (Name: string) => {
   return new Promise((resolve, reject) => {
     Connection.query(
-      `select gc.Name, gc.Description, bg1.Name as Gene1, bg2.Name as Gene2, bg3.Name as Gene3, bg4.Name as Gene4, bg5.Name as Gene5, bg6.Name as Gene6, gc.URL from genecombos gc left join basicgenetics bg1 on bg1.id = gc.Gene1 left join basicgenetics bg2 on bg2.id = gc.Gene2 left join basicgenetics bg3 on bg3.id = gc.Gene3 left join basicgenetics bg4 on bg4.id = gc.Gene4 left join basicgenetics bg5 on bg5.id = gc.Gene5 left join basicgenetics bg6 on bg6.id = gc.Gene6 WHERE gc.Name = ?`,
+      `SELECT gc.Name, gc.Description, gt1.Name AS GeneType1, bg1.Name AS Gene1, gt2.Name AS GeneType2, bg2.Name AS Gene2, gt3.Name AS GeneType3, bg3.Name AS Gene3, gt4.Name AS GeneType4, bg4.Name AS Gene4, gt5.Name AS GeneType5, bg5.Name AS Gene5, gt6.Name AS GeneType6, bg6.Name AS Gene6, gc.URL FROM genecombos gc LEFT JOIN basicgenetics bg1 ON bg1.ID = gc.Gene1 LEFT JOIN basicgenetics bg2 ON bg2.ID = gc.Gene2 LEFT JOIN basicgenetics bg3 ON bg3.ID = gc.Gene3 LEFT JOIN basicgenetics bg4 ON bg4.ID = gc.Gene4 LEFT JOIN basicgenetics bg5 ON bg5.ID = gc.Gene5 LEFT JOIN basicgenetics bg6 ON bg6.ID = gc.Gene6 LEFT JOIN genetypes gt1 ON gt1.ID = bg1.GeneType LEFT JOIN genetypes gt2 ON gt2.ID = bg2.GeneType LEFT JOIN genetypes gt3 ON gt3.ID = bg3.GeneType LEFT JOIN genetypes gt4 ON gt4.ID = bg4.GeneType LEFT JOIN genetypes gt5 ON gt5.ID = bg5.Genetype LEFT JOIN genetypes gt6 ON gt6.ID = bg6.GeneType WHERE gc.Name = ?`,
       [Name],
       (err, results) => {
         if (err) {
