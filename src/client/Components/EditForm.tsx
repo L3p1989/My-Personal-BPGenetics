1efaf3c91260c350510037c74da3cb3cd0fb1b3e
Patrick Hogan <phogan110@live.com>
2019-02-19 12:13:47 -0700

    removed `edit-container`

--- src/client/Components/EditForm.tsx
+++ src/client/Components/EditForm.tsx
@@ -26,110 +26,107 @@ class EditForm extends React.Component<IEditFormProps, IEditFormState> {
   render() {
     return (
       <>
-        <div className="edit-container rounded">
-          <form>
-            <div className="form-group">
-              <label>Name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Morph Name Here"
-              />
-            </div>
-            <div className="form-group">
-              <label>Description</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Morph Description Here"
-              />
-            </div>
-            <div className="form-group">
-              <label>Gene1</label>
-              <select className="form-control">
-                <option selected>None</option>
-                {this.state.basicCombos.map(basicCombo => {
-                  return (
-                    <option value={basicCombo.ID}>
-                      {basicCombo.ID}: {basicCombo.Name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Gene2</label>
-              <select className="form-control">
-                <option selected>None</option>
-                {this.state.basicCombos.map(basicCombo => {
-                  return (
-                    <option>
-                      {basicCombo.ID}: {basicCombo.Name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Gene3</label>
-              <select className="form-control">
-                <option selected>None</option>
-                {this.state.basicCombos.map(basicCombo => {
-                  return (
-                    <option>
-                      {basicCombo.ID}: {basicCombo.Name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Gene4</label>
-              <select className="form-control">
-                <option selected>None</option>
-                {this.state.basicCombos.map(basicCombo => {
-                  return (
-                    <option>
-                      {basicCombo.ID}: {basicCombo.Name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Gene5</label>
-              <select className="form-control">
-                <option selected>None</option>
-                {this.state.basicCombos.map(basicCombo => {
-                  return (
-                    <option>
-                      {basicCombo.ID}: {basicCombo.Name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Gene6</label>
-              <select className="form-control">
-                <option selected>None</option>
-                {this.state.basicCombos.map(basicCombo => {
-                  return (
-                    <option>
-                      {basicCombo.ID}: {basicCombo.Name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Picture URL</label>
-              <input type="text" className="form-control" />
-            </div>
-            <button className="save-btn">Save</button>
-            <button className="cancel-btn">Cancel</button>
-          </form>
-        </div>
+        <form>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Morph Name Here"
+            />
+          </div>
+          <div className="form-group">
+            <label>Description</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Morph Description Here"
+            />
+          </div>
+          <div className="form-group">
+            <label>Gene1</label>
+            <select className="form-control">
+              <option selected>None</option>
+              {this.state.basicCombos.map(basicCombo => {
+                return (
+                  <option value={basicCombo.ID}>
+                    {basicCombo.ID}: {basicCombo.Name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Gene2</label>
+            <select className="form-control">
+              <option selected>None</option>
+              {this.state.basicCombos.map(basicCombo => {
+                return (
+                  <option>
+                    {basicCombo.ID}: {basicCombo.Name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Gene3</label>
+            <select className="form-control">
+              <option selected>None</option>
+              {this.state.basicCombos.map(basicCombo => {
+                return (
+                  <option>
+                    {basicCombo.ID}: {basicCombo.Name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Gene4</label>
+            <select className="form-control">
+              <option selected>None</option>
+              {this.state.basicCombos.map(basicCombo => {
+                return (
+                  <option>
+                    {basicCombo.ID}: {basicCombo.Name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Gene5</label>
+            <select className="form-control">
+              <option selected>None</option>
+              {this.state.basicCombos.map(basicCombo => {
+                return (
+                  <option>
+                    {basicCombo.ID}: {basicCombo.Name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Gene6</label>
+            <select className="form-control">
+              <option selected>None</option>
+              {this.state.basicCombos.map(basicCombo => {
+                return (
+                  <option>
+                    {basicCombo.ID}: {basicCombo.Name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Picture URL</label>
+            <input type="text" className="form-control" />
+          </div>
+          <button className="save-btn">Save</button>
+        </form>
       </>
     );
   }
